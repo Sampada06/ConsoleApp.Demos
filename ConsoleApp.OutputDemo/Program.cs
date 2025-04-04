b20c7ae481c12054f9a4c815bcd4acad6a030ab0
DELL <DELL@DESKTOP-NNFO4UK>
2025-04-04 11:17:09 +0530

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I am from India.");
+Console.Write("Line1");
+Console.Write("Line2\n");
+
+//different data types 
+/* string - text 
+ * int - integer
+ * float - float, decimal,double
+ * bool -logical (Ture or False)
+ */
+
+String name = "Sampada";
+Console.WriteLine("I am " + name);  // String Concatination
+Console.WriteLine($"They call me {name}"); //String interpolation
+Console.WriteLine("I was given the name {0}",name);  //Formatted String
+
+int age = 24;
+Console.WriteLine("My age is : " + age);
---
+++ ConsoleApp.UserInput/Program.cs
@@ -0,0 +1,16 @@
+﻿//Declare variables
+String name = ""; //or string.Empty 
+int age = 0;
+int retirmentAge = 65;
+
+//prompt user for the input
+Console.WriteLine("Enter your name : ");
+name = Console.ReadLine();
+
+Console.WriteLine("Enter the age : ");
+age = Convert.ToInt32(Console.ReadLine());
+
+int workingYearsRemaining = retirmentAge - age;
+Console.WriteLine($"Full Name : {name}");
+Console.WriteLine($"Age : {age}");
+Console.WriteLine($"Working year remaining : {workingYearsRemaining}");
